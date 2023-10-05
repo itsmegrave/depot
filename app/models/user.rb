50d3473f69b25dad24f02e8c94babc7ccfeab95f
Samuel Grave <samuel@dinie.com>
2023-10-05 11:51:15 -0300

    :sparkles: Interation I4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  after_destroy ensure_an_admin_remains
+
+  class Error < StandardError; end
+
   has_secure_password
+
+  private
+
+  def ensure_an_admin_remains
+    raise Error, "Can't delete last user" if User.count.zero?
+  end
 end
